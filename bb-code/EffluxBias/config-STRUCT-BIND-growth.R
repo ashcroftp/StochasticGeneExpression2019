b7bf6d71484216bf6be5ba393672771e81e10a5a
Peter Ashcroft <peter.ashcroft@env.ethz.ch>
2019-08-06 23:09:25 +0200

    Missing Efflux bias config and params file

--- bb-code/EffluxBias/config-STRUCT-BIND-growth.R
+++ bb-code/EffluxBias/config-STRUCT-BIND-growth.R
@@ -1,4 +1,4 @@
-#' config-STRUCT-BIND-survival.R
+#' config-STRUCT-BIND-growth.R
 #' Author: Peter Ashcroft, ETH Zurich
 
 #' The parameters defined here will be passed to functions in the simulate.R file,
@@ -8,27 +8,17 @@
 #' Required parameters
 drug <- "ciprofloxacin"
 cell.type <- "STRUCT-BIND"
-output <- "survival"
-total.sims <- 1000
-sims.per.node <- 200
-queue <- "04:00"
+output <- "growth"
+total.sims <- 100
+sims.per.node <- 20
+queue <- "24:00"
 #' Variable parameters for which all combinations will be considered
 var.params <- expand.grid(
     mutant.effect = c(2, 10, 50, 200),
-    drug.outsideConcentration.MULT = c(0, 0.5, 0.7, seq(0.9,1.4,0.1), seq(1.5,3,0.25), seq(3.5,5,0.5)),
+    drug.outsideConcentration.MULT = 10^seq(-1, 1, 0.1),
     efflux.binom.param = seq(0.5, 1, 0.1)
 )
-#' Additional data required for higher drug concentrations
-var.params <- rbind(
-    var.params,
-    expand.grid(
-        mutant.effect = c(2, 10, 50, 200),
-        drug.outsideConcentration.MULT = c(seq(6,10,1), seq(12,20,2)),
-        efflux.binom.param = seq(0.5, 1, 0.1)
-    )
-)
 #' File identifier (this is the base name, to which we append text and a suffix to distinguish the output files)
-file.id <- paste(cell.type, "survival", sep = "_")
+file.id <- paste(cell.type, "growth", sep = "_")
 #' Location of the data directory (relative to the "home directory" bb-code/)
 data.directory <- "EffluxBias/zz-data/"
-
